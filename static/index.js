f065ede7f08dbeb3d629f29d27e52a9c94e3a4da
shaun919 <56931197+shaun919@users.noreply.github.com>
2022-01-30 06:26:57 +0000

    Update index.js

--- static/index.js
+++ static/index.js
@@ -442,9 +442,9 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
         }
 
         if (cps <= 5) return '试着好好练一下？';
-        if (cps <= 8) return 'TCL';
-        if (cps <= 10)  return 'TQL';
-        if (cps <= 15) return '您';
+        if (cps <= 8) return '老年人手速是吧';
+        if (cps <= 10)  return '哎哟不错噢';
+        if (cps <= 15) return '您是神仙吧';
         return '人？';
     }
 
