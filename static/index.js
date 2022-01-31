aa92d1e03622dd38be5768ad0ca400831aba5d30
shaun919 <56931197+shaun919@users.noreply.github.com>
2022-01-31 11:11:50 +0000

    Update index.js

--- static/index.js
+++ static/index.js
@@ -441,11 +441,11 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '试着好好练一下？';
-        if (cps <= 8) return '老年人手速是吧';
+        if (cps <= 5) return '这手速怎么抢红包';
+        if (cps <= 8) return '再吃口年夜饭';
         if (cps <= 10)  return '哎哟不错噢';
         if (cps <= 15) return '您是神仙吧';
-        return '人？';
+        return '此乃真人？';
     }
 
     function toStr(obj) {
